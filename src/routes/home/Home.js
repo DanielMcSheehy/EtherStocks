7e24cb17a2e25b77725d7621ad47c2a647d8d7f8
Daniel McSheehy <dsm140130@utdallas.edu>
2018-03-11 21:29:15 -0500

    Improving page layout

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -28,7 +28,8 @@ class Home extends React.Component {
     return (
       <div className={s.root}>
         <div >
-          <h1 style={{ marginLeft: '43%' }}>Featured</h1>
+          <h1 style={{ marginLeft: '44%' }}>Featured</h1>
+          <hr style={{ marginLeft: '4%', width: '90%' }}></hr>
           <ContractContainer />
         </div>
       </div>
